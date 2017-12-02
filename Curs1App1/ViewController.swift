7254b2d4ccda7c4169b98ff5fb852a5fa6d19f30
TelAcad2017 <cmudp2@gmail.com>
2017-12-02 08:57:52 +0200

    First Commin
    This is the first commit

---
+++ Curs1App1/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Curs1App1
+//
+//  Created by bla on 11/25/17.
+//  Copyright © 2017 bla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+ViewController instanta = ViewController()
